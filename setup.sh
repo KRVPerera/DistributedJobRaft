a63e43fbf917227d07d5ea93223d7db2773669a6
Rukshan Perera <2173530+KRVPerera@users.noreply.github.com>
2024-03-13 19:29:21 +0200

    fix: docker host address share

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ docker build . --build-arg CONFIG_FILE_PATH=config.xml -t djraft-node1:latest
 docker build . --build-arg CONFIG_FILE_PATH=config2.xml -t djraft-node2:latest
 docker build . --build-arg CONFIG_FILE_PATH=config3.xml -t djraft-node3:latest
 
-docker run -p 8082:8080 djraft-node2:latest
-docker run -p 8083:8080 djraft-node3:latest
-docker run -it --rm -p 8080:8080 djraft-node1:latest
+docker run --network host -p 8082:8080 djraft-node2:latest
+docker run --network host -p 8083:8080 djraft-node3:latest
+docker run --network host -it --rm -p 8080:8080 djraft-node1:latest
